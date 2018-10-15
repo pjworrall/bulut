46f0165850eccd7a789068e5833ee32e7b878dbb
Paul Worrall <paul.worrall@interition.net>
2018-10-15 19:58:15 +0100

    initial commit

---
+++ client/player.js
@@ -0,0 +1,73 @@
+import {Template} from 'meteor/templating';
+
+import {Howl} from 'howler';
+
+import {ReactiveVar} from 'meteor/reactive-var';
+
+import './player.html';
+
+Template.player.onCreated(function () {
+    this.sound = undefined;
+    this.position = new ReactiveVar();
+});
+
+Template.player.onRendered(function () {
+    this.sound = new Howl({
+        src: ['audio/rave_digger.mp3']
+    });
+
+    // this.playBtn = ;
+    // this.pauseBtn = ;
+
+});
+
+
+Template.player.helpers({
+
+    position() {
+        return Template.instance().position.get();
+    }
+
+});
+
+Template.player.events({
+    'click #playBtn'(event, instance) {
+        instance.sound.play();
+
+        // hide play button
+        instance.find('#playBtn').style.display = 'none';
+
+        // reveal the pause button
+        instance.find('#pauseBtn').style.display = 'block';
+        // reveal the annotation button
+        instance.find('#notesBtn').style.display = 'block';
+
+    },
+    'click #pauseBtn'(event, instance) {
+        instance.sound.pause();
+
+        // reveal the pause button
+        instance.find('#pauseBtn').style.display = 'none';
+
+        // hide play button
+        instance.find('#playBtn').style.display = 'block';
+    },
+    'click #notesBtn'(event, instance) {
+        instance.sound.pause();
+
+        let seek = instance.sound.seek();
+
+        let position = parseFloat(Math.round(seek * 100) / 100).toFixed(2);
+
+        instance.position.set(position) ;
+
+        console.log("position=" + position);
+
+        instance.find('#myModal').style.display = 'block';
+
+    },
+    'click #modalCloseBtn'(event, instance) {
+        instance.find('#myModal').style.display = 'none';
+        instance.sound.play();
+    }
+});
